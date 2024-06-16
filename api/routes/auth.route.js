4c0aac4477eff63ad64526e86c5f67681445419a
dilsha01 <mihirangangd.21@uom.lk>
2024-06-16 18:11:19 +0530

    Commit 17

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,9 +1,9 @@
 import express from "express";
-import {signup} from "../controllers/auth.controller.js";
+import {signup, signin} from "../controllers/auth.controller.js";
 
 const authRouter = express.Router();
 
 authRouter.post("/sign-up", signup);
-
+authRouter.post("/sign-in", signin);
 
 export default authRouter;
