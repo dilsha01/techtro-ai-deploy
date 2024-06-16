ff4ecd05c0383b49f8c94e542890f265cfca3376
dilsha01 <mihirangangd.21@uom.lk>
2024-06-16 18:16:38 +0530

    Commit 18

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -3,15 +3,13 @@ import bcryptjs from "bcryptjs";
 import jwt from "jsonwebtoken";
 import { errorHandler } from "../utils/error.js";
 
-const errorHandler = (status, message) => {
-    const error = new Error(message);
-    error.status = status;
-    return error;
-  };
+
+  
   export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
     const hashedPassword = bcryptjs.hashSync(password, 10);
     const newUser = new User({ username, email, password: hashedPassword });
+  
     try {
       await newUser.save();
       res.status(201).json({ message: "User created successfully!" });
@@ -23,7 +21,7 @@ const errorHandler = (status, message) => {
       next(error);
     }
   };
-
+  
   export const signin = async (req, res, next) => {
     const { email, password } = req.body;
   
@@ -42,4 +40,5 @@ const errorHandler = (status, message) => {
       next(error);
     }
   };
+  
   
